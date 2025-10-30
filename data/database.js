50f11b56425ba49fc04c3a5ae2a736549cb809de
Gift Ndoma <giftndoma87@gmail.com>
2025-10-30 13:05:43 +0100

    initial commit

---
+++ data/database.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MongoClient = require('mongodb').MongoClient;
+
+let database;
+
+const initDb = (callback) => {
+    if (database) {
+        console.log('Db is already initialised');
+        return callback(null, database);
+    }
+    MongoClient.connect(process.env.MONGODB_URL)
+    .then((client) => {
+        database = client;
+        callback(null, database);        
+    })
+    .catch((err) => {
+        callback(err);
+    });
+};
+
+const getDatabase = () => {
+    if (!database) {
+        throw Error("Database not initialised")
+    }
+
+    return database;
+};
+
+module.exports = {
+    initDb,
+    getDatabase
+}
